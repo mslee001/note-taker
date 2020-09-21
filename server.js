0fc25642138bc8e0079f76ebc2b8ba02ed68f35e
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-09-20 19:54:57 -0700

    Added get, post, and delete functionality

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var path = require("path");
+
+var fs = require("fs");
+const { json } = require("express");
+var data = fs.readFileSync('db/db.json', 'utf8');
+var notes = JSON.parse(data);
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static('./public/assets/'));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    console.log(notes);
+    return res.json(notes);
+    });
+
+app.post("/api/notes", function(req,res) {
+    var newNote = req.body;
+    newNote.id = notes.length + 1;
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+})
+
+app.delete("/api/notes/:id", function(req,res) {
+    var noteToDelete = parseInt(req.params.id);
+
+    for (var i = 0; i < notes.length; i++) {
+        if (noteToDelete === notes[i].id) {
+            notes.splice(i,1);
+            fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+            return res.json(notes);
+        }
+    }
+    return res.json(false);
+})
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
